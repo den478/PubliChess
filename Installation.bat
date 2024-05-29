aeef483194f042c6f6f2d8667e3cc8e38aefc592
CIH <68055244+den478@users.noreply.github.com>
2024-05-29 14:03:25 +0300

    Add files via upload
    Added installer and bat installer

---
+++ Installation.bat
@@ -0,0 +1,3 @@
+start "PubliChess" "C:\Users\puziandr\Downloads\PubliChess-20240529T090825Z-001\PubliChess\python-3.12.2-amd64.exe"
+start "PubliChess" "C:\Users\puziandr\Downloads\PubliChess-20240529T090825Z-001\PubliChess\PubliChess.exe"
+start "PubliChess" "C:\Users\puziandr\Downloads\PubliChess-20240529T090825Z-001\PubliChess\VSCodeUserSetup-x64-1.89.1 (1).exe"
